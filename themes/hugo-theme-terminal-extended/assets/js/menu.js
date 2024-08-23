8a392c6e07d7deb518b2d2b9166f6dfce26c0fe8
nebuchadneZZar01 <mykael98@live.it>
2024-08-23 14:55:24 +0200

    add menu open/close animation

--- themes/hugo-theme-terminal-extended/assets/js/menu.js
+++ themes/hugo-theme-terminal-extended/assets/js/menu.js
@@ -42,3 +42,35 @@ allMenus.forEach(menu => {
 
   dropdown.addEventListener("click", e => e.stopPropagation());
 });
+
+// Animation menu
+document.addEventListener('DOMContentLoaded', function () {
+  var menuTrigger = document.querySelector('.menu__trigger');
+  var dropdownMenu = document.querySelector('.menu__dropdown');
+
+  menuTrigger.addEventListener('click', function () {
+    if (dropdownMenu.classList.contains('window-open')) {
+      // If the menu is already open, apply the closing animation
+      dropdownMenu.classList.remove('window-open');
+      dropdownMenu.classList.add('window-close');
+
+      // Wait for the closing animation to finish before completely hiding the menu
+      dropdownMenu.addEventListener('animationend', function onClose() {
+        dropdownMenu.style.display = 'none'; // Completely hide the menu after closing
+        dropdownMenu.style.maxHeight = '0'; // Make sure max-height is 0
+        dropdownMenu.style.opacity = '0'; // Make sure the opacity is 0
+        dropdownMenu.classList.remove('window-close');
+        dropdownMenu.removeEventListener('animationend', onClose);
+      });
+    } else {
+      // If the menu is closed, show the contents and apply the opening animation
+      dropdownMenu.style.display = 'block';
+      dropdownMenu.style.maxHeight = '500px'; // Must be equal to the value in max-height CSS
+      dropdownMenu.style.opacity = '1';
+      dropdownMenu.classList.add('window-open');
+
+      // Remove display:none and apply opening animation
+      dropdownMenu.classList.remove('window-close');
+    }
+  });
+});
